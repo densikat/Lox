db22ac37d8548d3d5f437e5ceedbfcc0d0921dba
David Ensikat <densikat@gmail.com>
2023-06-27 09:28:54 +1000

    Finished Chapter 11 (resolver)

--- src/main/java/com/craftinginterpreters/lox/Environment.java
+++ src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -30,6 +30,18 @@ public class Environment {
         values.put(name, value);
     }
 
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
@@ -43,5 +55,9 @@ public class Environment {
 
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
+    }
+
+    public void assignAt(Integer distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
     }
 }
